cc3099ecc733af09aa445ad3b6d4edf8ba2e44c1
georgiavram <g.avram.m@gmail.com>
2020-04-21 12:04:52 +0300

    news reader

---
+++ app/src/main/java/com/AvramGeorgiana/newsreader/MainActivity.java
@@ -0,0 +1,21 @@
+package com.AvramGeorgiana.newsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AvramGeorgiana.newsreader.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
